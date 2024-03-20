cf553317224f29eafdf5ba91a16cff36a1b2404a
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-03-20 04:22:33 +0000

    isnatll laravel scout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Database\Factories\UserFactory;
 use Illuminate\Support\ServiceProvider;
+use Matchish\ScoutElasticSearch\Searchable\ImportSourceFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ImportSourceFactory::class, UserFactory::class);
+
     }
 
     /**
